7cff990ae7b2cfaa233711adc12f9baaea2f8230
Jeffrey Duda <jeff.duda@gmail.com>
2025-08-12 10:48:40 -0400

    ADD: synthseg stats

--- ss_stats_simple.py
+++ ss_stats_simple.py
@@ -7,10 +7,124 @@ import pathlib
 import SimpleITK as sitk
 import pandas as pd
 import numpy as np
-from totalsegmentator.statistics import get_radiomics_features
-from totalsegmentator.map_to_binary import class_map
+
 import tempfile
 import radiomics
+
+def synthseg_labels():
+    names = ["left cerebral white matter",
+             "left cerebral cortex",
+             "left lateral ventricle",
+             "left inferior lateral ventricle",
+             "left cerebellum white matter",
+             "left cerebellum cortex",
+             "left thalamus",
+             "left caudate",
+             "left putamen",
+             "left pallidum",
+             "3rd ventricle",
+             "4th ventricle",
+             "brain-stem",
+             "left hippocampus",
+             "left amygdala",
+             "left accumbens area",
+             "CSF","left ventral DC",
+             "right cerebral white matter",
+             "right cerebral cortex",
+             "right lateral ventricle",
+             "right inferior lateral ventricle",
+             "right cerebellum white matter",
+             "right cerebellum cortex",
+             "right thalamus",
+             "right caudate",
+             "right putamen",
+             "right pallidum",
+             "right hippocampus",
+             "right amygdala",
+             "right accumbens area",
+             "right ventral DC",
+             "left bankssts",
+             "left caudalanteriorcingulate",
+             "left caudalmiddlefrontal",
+             "left corpuscallosum ",
+             "left cuneus",
+             "left entorhinal",
+             "left fusiform",
+             "left inferiorparietal",
+             "left inferiortemporal",
+             "left isthmuscingulate",
+             "left lateraloccipital",
+             "left lateralorbitofrontal",
+             "left lingual",
+             "left medialorbitofrontal",
+             "left middletemporal",
+             "left parahippocampal",
+             "left paracentral",
+             "left parsopercularis",
+             "left parsorbitalis",
+             "left parstriangularis",
+             "left pericalcarine",
+             "left postcentral",
+             "left posteriorcingulate",
+             "left precentral",
+             "left precuneus",
+             "left rostralanteriorcingulate",
+             "left rostralmiddlefrontal",
+             "left superiorfrontal",
+             "left superiorparietal",
+             "left superiortemporal",
+             "left supramarginal",
+             "left frontalpole",
+             "left temporalpole",
+             "left transversetemporal",
+             "left insula",
+             "right unknown",
+             "right bankssts",
+             "right caudalanteriorcingulate",
+             "right caudalmiddlefrontal",
+             "right corpuscallosum",
+             "right cuneus",
+             "right entorhinal",
+             "right fusiform",
+             "right inferiorparietal",
+             "right inferiortemporal",
+             "right isthmuscingulate",
+             "right lateraloccipital",
+             "right lateralorbitofrontal",
+             "right lingual",
+             "right medialorbitofrontal",
+             "right middletemporal",
+             "right parahippocampal",
+             "right paracentral",
+             "right parsopercularis",
+             "right parsorbitalis",
+             "right parstriangularis",
+             "right pericalcarine",
+             "right postcentral",
+             "right posteriorcingulate",
+             "right precentral",
+             "right precuneus",
+             "right rostralanteriorcingulate",
+             "right rostralmiddlefrontal",
+             "right superiorfrontal",
+             "right superiorparietal",
+             "right superiortemporal",
+             "right supramarginal",
+             "right frontalpole",
+             "right temporalpole",
+             "right transversetemporal",
+             "right insula"]
+    values = [0,2,3,4,5,7,8,10,11,12,13,14,15,16,17,18,26,
+              24,28,41,42,43,44,46,47,49,50,51,52,53,54,58,60,
+              1001,1002,1003,1004,1005,1006,1007,1008,1009,1010,
+              1011,1012,1013,1014,1015,1016,1017,1018,1019,1020,
+              1021,1022,1023,1024,1025,1026,1027,1028,1029,1030,
+              1031,1032,1033,1034,1035,2000,2001,2002,2003,2004,
+              2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,
+              2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,
+              2025,2026,2027,2028,2029,2030,2031,2032,2033,2034,2035]
+
+    return( (names, values) )
 
 def split_path_into_subdirectories(path):
   """Splits a path into a list of its subdirectories."""
@@ -78,70 +192,6 @@ def value_list_to_string( val, seperator='|' ):
 
     return(ret_str)
 
-def get_json_stats(json_file):
-
-    dat=[]
-    f = open(json_file, 'r')
-    jdat = json.load(f)
-    f.close()
-
-    if 'Image' in jdat:
-        for k in jdat['Image']:
-            image_entry = jdat['Image'][k]
-            if isinstance(image_entry, list):
-                for (idx,ivalue) in enumerate(image_entry):
-                    row = get_data_row()
-                    row['label_system']='nifti'
-                    row['label_name']='header' 
-                    row['label_number']='NA'
-                    row['calculator']='itk'
-                    row['measure']=k
-                    row['metric']=str(idx)
-                    row['value']=str(ivalue)
-                    dat.append(row)
-            else:
-                row = get_data_row()
-                row['label_system']='nifti'
-                row['label_name']='header'
-                row['label_number']='NA'
-                row['calculator']='itk'
-                row['measure']=k
-                row['metric']='NA'
-                row['value']=str(image_entry)
-                dat.append(row)            
-
-    if 'Dicom' in jdat:
-        for k in jdat['Dicom']:
-            if k != 'InstanceList':
-                tag_entry = jdat['Dicom'][k]
-                if tag_entry['vr']=='SQ':
-                    sq = tag_entry['Value']
-                    for sq_entry in sq:
-                        for sub_k in sq_entry.keys():
-                            sub_entry = sq_entry[sub_k]
-                            for (idx, ivalue) in enumerate(sub_entry['Value']):
-                                row = get_data_row()
-                                row['label_system']='dicom'
-                                row['label_name']=tag_entry['Group']
-                                row['label_number']=tag_entry['Element']
-                                row['calculator']='dicom'
-                                row['measure']=k+'_'+sub_k
-                                row['metric']=str(idx)
-                                row['value']=str(ivalue).replace(",", "/")
-                                dat.append(row)
-                else:
-                    if tag_entry['Value'] is not None:
-                        for (idx, ivalue) in enumerate(tag_entry['Value']):
-                            row = get_data_row()
-                            row['label_system']='dicom'
-                            row['label_name']=tag_entry['Group']
-                            row['label_number']=tag_entry['Element']
-                            row['calculator']='dicom'
-                            row['measure']=k
-                            row['metric']=str(idx)
-                            row['value']=str(ivalue).replace(",", "/")
-                            dat.append(row)
-    return(dat)
 
 def mask_on_border(img):
     arr = sitk.GetArrayViewFromImage(img)
@@ -285,11 +335,7 @@ def main():
     my_parser.add_argument('-i', '--input', type=str, help='input ct image', required=True)
     my_parser.add_argument('-o', '--output', type=str, help='output csv', nargs='+', required=True)
     my_parser.add_argument('-s', '--seg', type=str, help='merged seg', required=True)
-    my_parser.add_argument('-e', '--effusion', type=str, help='effusion labels', required=False)
-    my_parser.add_argument('-m', '--meta', action='store_true', default=False, required=False)
     args = my_parser.parse_args()
-
-
 
     if not os.path.exists(args.seg):
         print("Input does not exist: "+args.seg)
@@ -309,25 +355,19 @@ def main():
     img_dirname = os.path.dirname(os.path.abspath(args.input))
 
     img_info = parse_image_filename(args.input)
-    print(img_info)
 
-    #json_name = os.path.join(img_dirname, img_basename+'.json')
-    #json_stats=[]
-    #if args.meta:
-    #    json_stats = get_json_stats(json_name)
+    (names, values) = synthseg_labels()
 
     print("get_radiomics_stats")
     t1 = time.perf_counter()
-    #rad_stats = get_radiomics_stats(args.input, args.seg, class_map['total'].values(),class_map['total'].keys())
-    rad_stats = get_radiomics_stats(img,seg,class_map['total'].values(),class_map['total'].keys())
-    #rad_stats=[]
+    rad_stats = get_radiomics_stats(img,seg,values,names)
     t2 = time.perf_counter()
     rad_time=t2-t1
     print(f"radiomics run time: {rad_time} seconds")
 
     print("get_simpleitk_stats")
     t3 = time.perf_counter()
-    sitk_stats = get_shape_stats(img,seg,class_map['total'].values(),class_map['total'].keys())
+    sitk_stats = get_shape_stats(img,seg,values,names)
     t4 = time.perf_counter()    
     itk_time=t4-t3
     print(f"SimpleITK run time: {itk_time} seconds")
@@ -335,10 +375,9 @@ def main():
     row_dat = []
     row_dat_eff = []
 
-    mydict = class_map['total']
-    for idx in class_map['total'].keys():
-        n = class_map['total'][idx]
-        if n in rad_stats:
+    for idx, label in enumerate(values):
+        nm = names[idx]
+        if nm in rad_stats:
             for stat in rad_stats[n].keys():
                 row = get_data_row()
                 row['id'] = img_info['pmbb_vision_id']
@@ -348,15 +387,15 @@ def main():
                 row['series_name'] = img_info['series_name']
                 row['image_filename'] = os.path.basename(args.input)
                 row['labels_filename'] = os.path.basename(args.seg)
-                row['label_name'] = n
-                row['label_number'] = idx
-                row['label_system'] = 'totalsegmentator_total_ct'
+                row['label_name'] = nm
+                row['label_number'] = label
+                row['label_system'] = 'synthseg'
                 row['calculator'] = 'pyradiomics'
                 row['measure'] = stat.split('_')[0]
                 row['metric'] = stat.split('_')[1]
-                row['value'] = str(rad_stats[n][stat])
+                row['value'] = str(rad_stats[nm][stat])
                 row_dat.append(row)
-        if n in sitk_stats:
+        if nm in sitk_stats:
             for stat in sitk_stats[n].keys():
                 row = get_data_row()
                 row['id'] = img_info['pmbb_vision_id']
@@ -366,46 +405,16 @@ def main():
                 row['series_name'] = img_info['series_name']
                 row['image_filename'] = os.path.basename(args.input)
                 row['labels_filename'] = os.path.basename(args.seg)
-                row['label_name'] = n
-                row['label_number'] = idx
-                row['label_system'] = 'totalsegmentator_total_ct'
+                row['label_name'] = nm
+                row['label_number'] = label
+                row['label_system'] = 'synthseg'
                 row['calculator'] = "simpleitk"
                 row['measure'] = stat.split('_')[0]
                 row['metric'] = stat.split('_')[1]
-                row['value'] = str(sitk_stats[n][stat])
+                row['value'] = str(sitk_stats[nm][stat])
 
                 row_dat.append(row)
-    
-    if args.effusion:
-        eff = sitk.ReadImage(args.effusion)
-        eff_names = ['pleural_effusion', 'pericardial_effusion']
-        eff_values = [2,3]
-        eff_stats = get_firstorder_stats(img,eff,eff_names,eff_values)
-        count=0
 
-        for n in eff_names:
-            idx=eff_values[count]
-            count=count+1
-
-            if n in eff_stats:
-                for stat in eff_stats[n].keys():
-                    row = get_data_row()
-                    row['id'] = img_info['pmbb_vision_id']
-                    row['accession_number'] = img_info['accession_number']
-                    row['study_uid'] = img_info['study_uid']
-                    row['series_number'] = img_info['series_number']
-                    row['series_name'] = img_info['series_name']
-                    row['image_filename'] = os.path.basename(args.input)
-                    row['labels_filename'] = os.path.basename(args.seg)
-                    row['label_name'] = n
-                    row['label_number'] = idx
-                    row['label_system'] = 'totalsegmentator_pleural_pericardial_effusion'
-                    row['calculator'] = "simpleitk"
-                    row['measure'] = stat.split('_')[0]
-                    row['metric'] = stat.split('_')[1]
-                    row['value'] = str(eff_stats[n][stat])
-
-                    row_dat_eff.append(row)
 
     df = pd.DataFrame.from_dict(row_dat)
     df_eff = pd.DataFrame.from_dict(row_dat_eff)
